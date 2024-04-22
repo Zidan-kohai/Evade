1c8412e928f6db534e11c05aae1e3d724a8ed3d5
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-04-22 14:09:59 +0300

    PlayerController is ready

--- Assets/2_Scripts/Player/PlayerController.cs
+++ Assets/2_Scripts/Player/PlayerController.cs
@@ -1,52 +1,113 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float mouseSensitivity = 100f;
-    public float jumpHeight = 2f; 
-    public float gravity = -9.81f;
+    [Header("Transform")]
+    [SerializeField] private float startSpeed = 1f;
+    [SerializeField] private float currrentSpeed = 1f;
+    [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float mouseSensitivity = 100f;
+    [SerializeField] private float minVerticalRotateClamp = 100f;
+    [SerializeField] private float maxVerticalRotateClamp = 100f;
+    [SerializeField] private float verticalRotation = 0f;
+    [SerializeField] private float jumpHeight = 2f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform groundChekcRaycastOrigin; 
+    [SerializeField] private float groundChekcRaycastHeight; 
+    [SerializeField] private LayerMask groundLayer; 
+    [SerializeField] private bool isOnGround;
+    [SerializeField] private Vector3 velocity;
 
+    [Header("Components")]
     [SerializeField] private InputManager inputManager;
     private CharacterController characterController;
-    private float verticalRotation = 0f;
 
-    void Start()
+    private void Start()
     {
         characterController = GetComponent<CharacterController>();
 
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    void Update()
+    private void Update()
+    {
+        Rotate();
+
+        Move();
+
+        CheckGround();
+
+        Jump();
+        
+        ApplyGravity();
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    private void Rotate()
     {
         float mouseX = inputManager.GetMouseDeltaX * mouseSensitivity * Time.deltaTime;
         transform.Rotate(Vector3.up * mouseX);
 
         float mouseY = inputManager.GetMouseDeltaY * mouseSensitivity * Time.deltaTime;
         verticalRotation -= mouseY;
-        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f); // Ограничиваем поворот вверх и вниз
+        verticalRotation = Mathf.Clamp(verticalRotation, minVerticalRotateClamp, maxVerticalRotateClamp);
         Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
 
+    private void Move()
+    {
         float moveHorizontal = inputManager.GetMoveHorizontal;
         float moveVertical = inputManager.GetMoveVertical;
 
+        
         Vector3 move = transform.right * moveHorizontal + transform.forward * moveVertical;
-        characterController.Move(move * moveSpeed * Time.deltaTime);
 
-        if (inputManager.GetSpace && characterController.isGrounded)
+        velocity.x = move.x * currrentSpeed;
+        velocity.z = move.z * currrentSpeed;
+
+        if(moveHorizontal == 0 && moveVertical == 0)
         {
-            float jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
-            characterController.Move(Vector3.up * jumpVelocity * Time.deltaTime);
+            currrentSpeed = startSpeed;
         }
-        if (!characterController.isGrounded)
+        else
         {
-            Gravity();
+            currrentSpeed += Time.deltaTime;
+            currrentSpeed = Mathf.Clamp(currrentSpeed, startSpeed, maxSpeed);
         }
     }
 
-    public void Gravity()
+    private void Jump()
     {
-        characterController.Move(Vector3.up * gravity * Time.deltaTime);
+        if (inputManager.GetSpace && isOnGround)
+        {
+            float jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+            velocity.y += jumpVelocity;
+            isOnGround = false;
+        }
+    }
+
+    public void ApplyGravity()
+    {
+        if (!isOnGround)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            velocity.y = 0;
+        }
+    }
+
+    private void CheckGround()
+    {
+        isOnGround = Physics.Raycast(groundChekcRaycastOrigin.position, Vector3.down, groundChekcRaycastHeight, groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(groundChekcRaycastOrigin.position, Vector3.down * groundChekcRaycastHeight);
     }
 }
