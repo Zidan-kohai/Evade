33940ffd71940ba8fc3953987948ea3d552b7649
Zayniddin Kobilov <102047747+Zidan-kohai@users.noreply.github.com>
2024-05-29 14:21:58 +0300

    edit almost all ui

--- Assets/2_Scripts/Enemy/Enemy.cs
+++ Assets/2_Scripts/Enemy/Enemy.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
-using static Unity.Burst.Intrinsics.X86;
+//using static Unity.Burst.Intrinsics.X86;
 
 public class Enemy : MonoBehaviour, IEnemy, ISee, IHumanoid
 {
--- Assets/2_Scripts/SimpleShop/ShopChapterSwithcer.cs
+++ Assets/2_Scripts/SimpleShop/ShopChapterSwithcer.cs
@@ -1,26 +1,37 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopChapterSwithcer : MonoBehaviour
 {
     [SerializeField] private RectTransform lightSubjectHandler;
+    [SerializeField] private Image lightSubjectChapterButtonImage;
     [SerializeField] private RectTransform itemSubjectHandler;
+    [SerializeField] private Image itemSubjectChapterButtonImage;
     [SerializeField] private RectTransform boosterSubjectHandler;
+    [SerializeField] private Image boosterSubjectChapterButtonImage;
     [SerializeField] private RectTransform accessorySubjectHandler;
+    [SerializeField] private Image accessorySubjectChapterButtonImage;
 
     [SerializeField] private RectTransform equipButton;
     [SerializeField] private Vector3 equipButtonOpenPosition = new Vector3(0, 200, 0);
     [SerializeField] private Vector3 equipButtonClosePosition = new Vector3(0, 15, 0);
     [SerializeField] private ShowChapter openedChapter;
 
+    [SerializeField] private Sprite openChapterSprite;
+    [SerializeField] private Sprite closeChapterSprite;
+
     #region Light
     public void OpenLightChapter()
     {
         CloseCurrentOpenChapter();
         lightSubjectHandler.gameObject.SetActive(true);
+        lightSubjectChapterButtonImage.sprite = openChapterSprite;
         openedChapter = ShowChapter.Light;
     }
     private void CloseLightChapter()
     {
+        lightSubjectChapterButtonImage.sprite = closeChapterSprite;
         lightSubjectHandler.gameObject.SetActive(false);
     }
     #endregion
@@ -29,11 +40,13 @@ public class ShopChapterSwithcer : MonoBehaviour
     public void OpenItemChapter()
     {
         CloseCurrentOpenChapter();
+        itemSubjectChapterButtonImage.sprite = openChapterSprite;
         itemSubjectHandler.gameObject.SetActive(true);
         openedChapter = ShowChapter.Item;
     }
     private void CloseItemChapter()
     {
+        itemSubjectChapterButtonImage.sprite = closeChapterSprite;
         itemSubjectHandler.gameObject.SetActive(false);
     }
     #endregion
@@ -42,12 +55,14 @@ public class ShopChapterSwithcer : MonoBehaviour
     public void OpenBoosterChapter()
     {
         CloseCurrentOpenChapter();
+        boosterSubjectChapterButtonImage.sprite = openChapterSprite;
         boosterSubjectHandler.gameObject.SetActive(true);
         equipButton.anchoredPosition = equipButtonOpenPosition;
         openedChapter = ShowChapter.Booster;
     }
     private void CloseBoosterChapter()
     {
+        boosterSubjectChapterButtonImage.sprite = closeChapterSprite;
         equipButton.anchoredPosition = equipButtonClosePosition;
         boosterSubjectHandler.gameObject.SetActive(false);
     }
@@ -57,11 +72,13 @@ public class ShopChapterSwithcer : MonoBehaviour
     public void OpenAccessoryChapter()
     {
         CloseCurrentOpenChapter();
+        accessorySubjectChapterButtonImage.sprite = openChapterSprite;
         accessorySubjectHandler.gameObject.SetActive(true);
         openedChapter = ShowChapter.Accessory;
     }
     private void CloseAccessoryChapter()
     {
+        accessorySubjectChapterButtonImage.sprite = closeChapterSprite;
         accessorySubjectHandler.gameObject.SetActive(false);
     }
     #endregion
