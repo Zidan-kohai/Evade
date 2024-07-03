e0abbee0a69981c37bf3e7e6c69332985192c9be
manuchehr <112807695+manuchehr-olimov-dev@users.noreply.github.com>
2024-07-04 00:06:22 +0300

    Update BoosterController.cs
    посмотри чтоль

--- Assets/2_Scripts/Booster/BoosterController.cs
+++ Assets/2_Scripts/Booster/BoosterController.cs
@@ -8,13 +8,12 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngineInternal;
 
-public class BoosterController : MonoBehaviour
-{
+public class BoosterController : MonoBehaviour {
     private static BoosterController instance;
 
 
     [SerializeField] private InputManager inputManager;
-
+    
     [SerializeField] private List<BoosterItem> boosterItem;
     [SerializeField] private List<MyDictionary> Boosters;
 
@@ -60,10 +59,9 @@ public class BoosterController : MonoBehaviour
     [Header("Meat")]
     [SerializeField] private int meatMaxUseCount = 3;
     [SerializeField] private int meatUsedCount = 0;
-    private void Awake()
-    {
-        if(instance != null)
-        {
+    
+    private void Awake() {
+        if(instance != null) {
             Destroy(gameObject);
         }
 
@@ -72,19 +70,17 @@ public class BoosterController : MonoBehaviour
         //Need refactoring
 
 
-        for (int i = 0; i < Geekplay.Instance.PlayerData.CurrentBoosterKeys.Count; i++)
-        {
-            if (Geekplay.Instance.PlayerData.CurrentBoosterKeys[i] == -1)
-            {
+        // Object geekPlayData = Geekplay.Instance.PlayerData;
+        for (int i = 0; i < geekPlayData.CurrentBoosterKeys.Count; i++) {
+            if (geekPlayData.CurrentBoosterKeys[i] == -1) {
                 continue;
             }
 
-            for (int j = i; j < boosterItem.Count; j++)
-            {
+            for (int j = i; j < boosterItem.Count; j++) {
                 bool result = false;
-                foreach (MyDictionary booster in Boosters)
-                {
-                    if (Geekplay.Instance.PlayerData.CurrentBoosterKeys[i] == booster.data.indexOnPlayer && Geekplay.Instance.PlayerData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value != 0)
+                foreach (MyDictionary booster in Boosters) {
+                    if (geekPlayData.CurrentBoosterKeys[i] == booster.data.indexOnPlayer && 
+                        geekPlayData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value != 0) 
                     {
                         boosterItem[j].gameObject.SetActive(true);
                         boosterItem[j].image.sprite = booster.data.mainIcon;
@@ -94,56 +90,50 @@ public class BoosterController : MonoBehaviour
 
                         int index = i;
                         int boosterIndex = j;
-                        boosterItem[j].boostEvent.AddListener(() =>
-                        {
-                            if(Geekplay.Instance.PlayerData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value == 0)
-                            {
+                        boosterItem[j].boostEvent.AddListener(() => {
+                            if(geekPlayData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value == 0) {
                                 return;
                             }
-
-
-                            if(booster.boosterEvent?.Invoke(booster.data) == true)
-                            {
-                                Geekplay.Instance.PlayerData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value--;
-                                boosterItem[j].remainingCounView.text = Geekplay.Instance.PlayerData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value.ToString();
+                            
+                            if(booster.boosterEvent?.Invoke(booster.data) == true) {
+                                geekPlayData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value--;
+                                boosterItem[j].remainingCounView.text = geekPlayData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value.ToString();
                             }
 
                             DailyExerciseController.Instance.SetProgress(Days.Day3, 2);
 
-                            if (Geekplay.Instance.PlayerData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value == 0)
-                            {
+                            if (geekPlayData.BuyedBoosterID.GetByKey(booster.data.indexOnPlayer).value == 0) {
                                 boosterItem[boosterIndex].gameObject.SetActive(false);
                             }
                         });
-
+                        
                         result = true;
                         break;
                     }
                 }
-                if (result) break;
+                if (result) { break; };
             }
         }
 
-        foreach (MyDictionary booster in Boosters)
-        {
-            if (Geekplay.Instance.PlayerData.CurrentEquipedItemID == booster.data.indexOnPlayer && booster.data.type == SubjectType.Item)
-            {
+        foreach (MyDictionary booster in Boosters) {
+            if (geekPlayData.CurrentEquipedItemID == booster.data.indexOnPlayer && booster.data.type == SubjectType.Item) {
                 meatUsedCount = meatMaxUseCount;
 
                 boosterItem[5].gameObject.SetActive(true);
                 boosterItem[5].image.sprite = booster.data.mainIcon;
                 boosterItem[5].remainingCounView.text = meatUsedCount.ToString();
 
-                boosterItem[5].boostEvent.AddListener(() =>
-                {
-                    if (meatUsedCount <= 0) return;
+                boosterItem[5].boostEvent.AddListener(() => {
+                    if (meatUsedCount <= 0) { return; }
 
                     meatUsedCount--;
                     boosterItem[5].remainingCounView.text = meatUsedCount.ToString();
 
                     booster.boosterEvent?.Invoke(booster.data);
 
-                    if(meatUsedCount <= 0) boosterItem[5].gameObject?.SetActive(false);
+                    if(meatUsedCount <= 0) { 
+                        boosterItem[5].gameObject?.SetActive(false); 
+                    }
                 });
                 break;
             }
@@ -152,8 +142,7 @@ public class BoosterController : MonoBehaviour
         Subcscribe();
     }
 
-    private void Subcscribe()
-    {
+    private void Subcscribe() {
         Boosters[0].boosterEvent += ColaBoost;
         Boosters[1].boosterEvent += WandOfLightBoost;
         Boosters[2].boosterEvent += Barrier;
@@ -163,10 +152,10 @@ public class BoosterController : MonoBehaviour
         Boosters[6].boosterEvent += playerController.CreateBait;
     }
 
-    private void Update()
-    {
-        if (realyPlayer.IsFallOrDeath()) return;
+    private void Update() {
+        if (realyPlayer.IsFallOrDeath()) { return; }
 
+        // слишком много условий. вынеси логику в отдельный метод
         if (inputManager.GetIs1 && boosterItem[0].gameObject.activeSelf)
         {
             boosterItem[0].boostEvent?.Invoke();
@@ -198,22 +187,18 @@ public class BoosterController : MonoBehaviour
         }
     }
 
-    public static void AddRealyPlayerST(IRealyPlayer realyPlayer)
-    {
+    public static void AddRealyPlayerST(IRealyPlayer realyPlayer) {
         instance.AddRealyPlayer(realyPlayer);
     }
 
-    private void AddRealyPlayer(IRealyPlayer realyPlayer)
-    {
+    private void AddRealyPlayer(IRealyPlayer realyPlayer) {
         this.realyPlayer = realyPlayer;
     }
 
     #region Cola
 
-    public bool ColaBoost(ShopItemData data)
-    {
+    public bool ColaBoost(ShopItemData data) {
         ColaSequence.Kill();
-
         cocacolaAudio.Play();
 
         float deltaUp = realyPlayer.SetMaxSpeedOnUp(50);
@@ -224,11 +209,9 @@ public class BoosterController : MonoBehaviour
             .OnKill(() => ColaBoostDiactivate(deltaUp, deltaFall));
 
         return true;
-
     }
 
-    private void ColaBoostDiactivate(float deltaUp, float deltaFall)
-    {
+    private void ColaBoostDiactivate(float deltaUp, float deltaFall) {
         realyPlayer.SetMaxSpeedOnUp((int)-deltaUp, false);
         realyPlayer.SetMaxSpeedOnFall((int)-deltaFall, false);
     }
@@ -237,9 +220,14 @@ public class BoosterController : MonoBehaviour
 
     #region WangOfLight
 
-    public bool WandOfLightBoost(ShopItemData data)
-    {
-        Instantiate(WandOfLightPrefab, realyPlayerTransform.position + realyPlayerTransform.forward * 5 + new Vector3(0, 5, 0), Quaternion.identity, null);
+    public bool WandOfLightBoost(ShopItemData data) {
+        Instantiate(
+            WandOfLightPrefab, 
+            realyPlayerTransform.position + realyPlayerTransform.forward * 5 + new Vector3(0, 5, 0), 
+            Quaternion.identity, 
+            null
+        );
+        
         WandOfLightAudio.Play();
         return true;
     }
@@ -248,8 +236,7 @@ public class BoosterController : MonoBehaviour
 
     #region Barrier
 
-    public bool Barrier(ShopItemData data)
-    {
+    public bool Barrier(ShopItemData data) {
         Ray ray = mainCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
 
@@ -269,24 +256,21 @@ public class BoosterController : MonoBehaviour
 
         barrierUsedCount++;
 
-        if(barrierUsedCount >= 10)
-        {
+        if(barrierUsedCount >= 10) {
             DailyExerciseController.Instance.SetProgress(Days.Day5, 3);
         }
         barrierAudio.Play();
         return true;
     }
 
-    private void DiactivateBarrier(GameObject barrierInstance)
-    {
+    private void DiactivateBarrier(GameObject barrierInstance) {
         Destroy(barrierInstance);
     }
 
     #endregion
 
     #region Sensor
-    public bool Sensor(ShopItemData data)
-    {
+    public bool Sensor(ShopItemData data) {
         Ray ray = mainCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
 
@@ -301,32 +285,28 @@ public class BoosterController : MonoBehaviour
         return true;
     }
 
-    private void DiactivateSensor(GameObject sensor)
-    {
+    private void DiactivateSensor(GameObject sensor) {
         Destroy(sensor);
     }
 
     #endregion
 
     #region Mine
-    public bool Mine(ShopItemData data)
-    {
+    public bool Mine(ShopItemData data) {
         Ray ray = mainCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
 
         Physics.Raycast(ray.origin, ray.direction, out hit, 100, barrierSpawnable, QueryTriggerInteraction.Ignore);
         
 
-        if (hit.normal != Vector3.up)
-        {
+        if (hit.normal != Vector3.up) {
             ray.origin = hit.point + hit.normal;
             ray.direction = Vector3.down;
 
             Physics.Raycast(ray.origin, ray.direction, out hit, 100, barrierSpawnable, QueryTriggerInteraction.Ignore);
         }
 
-        if (hit.collider == null || hit.collider.gameObject.layer != 6)
-        {
+        if (hit.collider == null || hit.collider.gameObject.layer != 6) {
             Debug.Log("Don`t Find Ground");
             return false;
         }
@@ -341,32 +321,27 @@ public class BoosterController : MonoBehaviour
         return true;
     }
 
-    private void DiactivateMine(GameObject mine)
-    {
-        if(mine != null)
-            Destroy(mine);
+    private void DiactivateMine(GameObject mine) {
+        if(mine != null) { Destroy(mine); }
     }
     #endregion
 
     #region Teleport
-    public bool TeleportActivate(ShopItemData data)
-    {
+    public bool TeleportActivate(ShopItemData data) {
         Ray ray = mainCamera.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
 
         Physics.Raycast(ray.origin, ray.direction, out hit, 100, barrierSpawnable, QueryTriggerInteraction.Ignore);
 
 
-        if (hit.normal != Vector3.up)
-        {
+        if (hit.normal != Vector3.up) {
             ray.origin = hit.point + hit.normal * 5;
             ray.direction = Vector3.down;
 
             Physics.Raycast(ray.origin, ray.direction, out hit, 100, barrierSpawnable, QueryTriggerInteraction.Ignore);
         }
 
-        if (hit.collider == null || hit.collider.gameObject.layer != 6)
-        {
+        if (hit.collider == null || hit.collider.gameObject.layer != 6) {
             Debug.Log("Don`t Find Ground");
             return false;
         }
@@ -379,13 +354,11 @@ public class BoosterController : MonoBehaviour
         return true;
     }
 
-    public void Teleport(TeleportPoint point, IPlayer player)
-    {
-        if (teleportsInstance.Count == 1) return; 
+    public void Teleport(TeleportPoint point, IPlayer player) {
+        if (teleportsInstance.Count == 1) { return; }
 
         int rand = UnityEngine.Random.Range(0, teleportsInstance.Count);
-        if(point == teleportsInstance[rand])
-        {
+        if(point == teleportsInstance[rand]) {
             Teleport(point, player);
             return;
         }
@@ -395,8 +368,7 @@ public class BoosterController : MonoBehaviour
     #endregion 
 
     [Serializable]
-    public class MyDictionary
-    {
+    public class MyDictionary {
         public ShopItemData data;
         public Predicate<ShopItemData> boosterEvent;
     }
